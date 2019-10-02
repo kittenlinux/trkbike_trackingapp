02ae1e25ece1fe8c4983ecc9b26c7d1b737f2c6b
Pummarin Jomkoa <kittenlinux@users.noreply.github.com>
2019-10-02 17:07:04 +0700

    revert last commit

--- android/app/src/main/java/com/trkbiketracking/MainApplication.java
+++ android/app/src/main/java/com/trkbiketracking/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.List;
 
+import org.pgsqlite.SQLitePluginPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
