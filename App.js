e1d7b6ad8a4abb17f17b6f8213f170ecb8f4d389
Pummarin Jomkoa <1388309+kittenlinux@users.noreply.github.com>
2019-11-25 11:32:11 +0700

    Reformating code

--- App.js
+++ App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Alert, BackHandler, Linking, PermissionsAndroid, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { CameraKitCameraScreen } from 'react-native-camera-kit';
 import DeviceInfo from 'react-native-device-info';
- 
+
 export default class App extends Component {
   constructor() {
     super();
- 
+
     this.state = {
       QR_Code_Value: '',
       Start_Scanner: false,
@@ -16,22 +16,22 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    DeviceInfo.getMacAddress().then(mac => { mac_addr = JSON.stringify(mac).slice(1, JSON.stringify(mac).length-1); });
+    DeviceInfo.getMacAddress().then(mac => { mac_addr = JSON.stringify(mac).slice(1, JSON.stringify(mac).length - 1); });
     this.backHandler = BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
   }
 
   componentWillUnmount() {
-    this.backHandler.remove()
+    this.backHandler.remove();
   }
 
   handleBackPress = () => {
-    if(!this.state.Start_Scanner){
+    if (!this.state.Start_Scanner) {
       Alert.alert(
         'ออกจากโปรแกรม',
         'คุณต้องการออกไปยังหน้าหลักหรือไม่ ?',
         [
-          {text: 'ไม่ใช่', onPress: () => console.log('ยกเลิก'), style: 'cancel'},
-          {text: 'ใช่', onPress: () => BackHandler.exitApp()},
+          { text: 'ไม่ใช่', onPress: () => console.log('ยกเลิก'), style: 'cancel' },
+          { text: 'ใช่', onPress: () => BackHandler.exitApp() },
         ],
         { cancelable: true });
     }
@@ -39,23 +39,23 @@ export default class App extends Component {
       this.setState({ Start_Scanner: false });
     return true;
   }
- 
+
   openLink_in_browser = () => {
     Linking.openURL(this.state.QR_Code_Value);
   }
- 
+
   onQR_Code_Scan_Done = (QR_Code) => {
     var qrdata, isJSON = '1';
 
-    try{
+    try {
       qrdata = JSON.parse(QR_Code);
     }
     catch{
-      Alert.alert('ผิดพลาด','รูปแบบคิวอาร์โค้ดไม่ถูกต้อง โปรดตรวจสอบข้อมูลอีกครั้ง');
+      Alert.alert('ผิดพลาด', 'รูปแบบคิวอาร์โค้ดไม่ถูกต้อง โปรดตรวจสอบข้อมูลอีกครั้ง');
       isJSON = '0';
     }
 
-    if(isJSON == '1'){
+    if (isJSON == '1') {
       qrdata.macAddr = mac_addr;
 
       fetch('https://www.trackmycars.net/bike/Api/V1/register_check/', {
@@ -66,77 +66,77 @@ export default class App extends Component {
         },
         body: JSON.stringify(qrdata),
       })
-      .then((response) => response.json())
-      .then((responseData) => {                   
-        this.setState({
-          loading: false
+        .then((response) => response.json())
+        .then((responseData) => {
+          this.setState({
+            loading: false
+          });
+          console.log(JSON.stringify(responseData));
+          console.log(responseData);
+
+          var bikedata = responseData.data;
+
+          if (responseData.code == 'SUCCESS') {
+            if (bikedata.mac_status === '0')
+              mac_msg = 'ยังไม่มีการผูกอุปกรณ์ใด ๆ เข้ากับรถคันนี้\nที่อยู่ MAC Address ' + mac_addr + ' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้';
+            else if (bikedata.mac_status === '1')
+              mac_msg = 'อุปกรณ์นี้คืออุปกรณ์เดิมที่ท่านเคยผูกมาก่อนหน้านี้\nที่อยู่ MAC Address ' + mac_addr + ' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้อีกครั้ง';
+            else if (bikedata.mac_status === '2')
+              mac_msg = 'มีการผูกอุปกรณ์อื่นเข้ากับรถจักรยานยนต์คันนี้อยู่แล้ว !\nที่อยู่ MAC Address ' + mac_addr + ' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้แทนที่อุปกรณ์เดิม';
+
+            Alert.alert(
+              'ตรวจสอบข้อมูล',
+              responseData.message +
+              '\n\nผู้ใช้งาน : ' + bikedata.username +
+              '\nหมายเลขทะเบียน : ' + bikedata.plate +
+              '\n\nยี่ห้อ รุ่น : ' + bikedata.model +
+              '\nสี : ' + bikedata.color +
+              '\n\n' + mac_msg +
+              '\n\nยืนยันการผูกอุปกรณ์เข้ากับรถจักรยานยนต์ ?',
+              [
+                { text: 'ยืนยัน', onPress: () => console.log('ยืนยัน') },
+                { text: 'ยกเลิก', onPress: () => console.log('ยกเลิก'), style: 'cancel' },
+              ],
+              { cancelable: false }
+            );
+          } else if (responseData.code == 'FAIL') {
+            Alert.alert(
+              'ผิดพลาด',
+              responseData.message + '\n\nโปรดตรวจสอบข้อมูลอีกครั้ง'
+            );
+          }
+
+
         });
-        console.log(JSON.stringify(responseData));
-        console.log(responseData);
-
-        var bikedata = responseData.data;
-
-        if(responseData.code=='SUCCESS'){
-          if(bikedata.mac_status==='0')
-            mac_msg='ยังไม่มีการผูกอุปกรณ์ใด ๆ เข้ากับรถคันนี้\nที่อยู่ MAC Address '+mac_addr+' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้';
-          else if(bikedata.mac_status==='1')
-            mac_msg='อุปกรณ์นี้คืออุปกรณ์เดิมที่ท่านเคยผูกมาก่อนหน้านี้\nที่อยู่ MAC Address '+mac_addr+' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้อีกครั้ง';
-          else if(bikedata.mac_status==='2')
-            mac_msg='มีการผูกอุปกรณ์อื่นเข้ากับรถจักรยานยนต์คันนี้อยู่แล้ว !\nที่อยู่ MAC Address '+mac_addr+' จะถูกผูกเข้ากับรถจักรยานยนต์คันนี้แทนที่อุปกรณ์เดิม';
-
-          Alert.alert(
-            'ตรวจสอบข้อมูล',
-            responseData.message+
-            '\n\nผู้ใช้งาน : '+bikedata.username+
-            '\nหมายเลขทะเบียน : '+bikedata.plate+
-            '\n\nยี่ห้อ รุ่น : '+bikedata.model+
-            '\nสี : '+bikedata.color+
-            '\n\n'+mac_msg+
-            '\n\nยืนยันการผูกอุปกรณ์เข้ากับรถจักรยานยนต์ ?',
-            [
-              {text: 'ยืนยัน', onPress: () => console.log('ยืนยัน')},
-              {text: 'ยกเลิก', onPress: () => console.log('ยกเลิก'), style: 'cancel'},
-            ],
-            { cancelable: false }
-          );
-        } else if(responseData.code=='FAIL'){
-          Alert.alert(
-            'ผิดพลาด',
-            responseData.message+'\n\nโปรดตรวจสอบข้อมูลอีกครั้ง'
-          );
-        }
-
-        
-      });
     }
 
     this.setState({ QR_Code_Value: QR_Code });
     this.setState({ Start_Scanner: false });
   }
- 
-  open_QR_Code_Scanner=()=> {
+
+  open_QR_Code_Scanner = () => {
     var that = this;
- 
+
     if (Platform.OS === 'android') {
       async function requestCameraPermission() {
         try {
           const granted = await PermissionsAndroid.request(
             PermissionsAndroid.PERMISSIONS.CAMERA, {
-              title: 'ขอสิทธิ์การใช้กล้องถ่ายรูป',
-              message: 'แอปพลิเคชันต้องการขอสิทธิ์เพื่อใช้งานกล้อง',
-              buttonPositive: 'ตกลง'
-            }
+            title: 'ขอสิทธิ์การใช้กล้องถ่ายรูป',
+            message: 'แอปพลิเคชันต้องการขอสิทธิ์เพื่อใช้งานกล้อง',
+            buttonPositive: 'ตกลง'
+          }
           )
           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
- 
+
             that.setState({ QR_Code_Value: '' });
             that.setState({ Start_Scanner: true });
-            
+
           } else {
-            Alert.alert("ผิดพลาด","การขอสิทธิ์เพื่อใช้งานกล้องถูกปฏิเสธ");
+            Alert.alert("ผิดพลาด", "การขอสิทธิ์เพื่อใช้งานกล้องถูกปฏิเสธ");
           }
         } catch (err) {
-          Alert.alert("ผิดพลาด","พบปัญหาในการขอสิทธิ์เพื่อใช้งานกล้อง");
+          Alert.alert("ผิดพลาด", "พบปัญหาในการขอสิทธิ์เพื่อใช้งานกล้อง");
           console.warn(err);
         }
       }
@@ -147,29 +147,29 @@ export default class App extends Component {
     }
   }
 
-  set_Status_OnOff=()=> {
+  set_Status_OnOff = () => {
     Alert.alert(
       'ข้อมูล',
-      'หมายเลข MAC Address ของคุณคือ '+mac_addr,
+      'หมายเลข MAC Address ของคุณคือ ' + mac_addr,
     );
   }
 
-  onBottomButtonPressed=()=>{
+  onBottomButtonPressed = () => {
     this.setState({ Start_Scanner: false });
   }
 
   render() {
     if (!this.state.Start_Scanner) {
- 
+
       return (
         <View style={styles.MainContainer}>
- 
+
           <Text style={{ fontSize: 22, textAlign: 'center' }}>ยินดีต้อนรับสู่ ลูกข่ายติดตามรถจักรยานยนต์ Track My Bikes</Text>
- 
+
           <Text style={styles.QR_text}>
             {this.state.QR_Code_Value ? 'Scanned QR Code: ' + this.state.QR_Code_Value : ''}
           </Text>
- 
+
           {this.state.QR_Code_Value.includes("http") ?
             <TouchableOpacity
               onPress={this.openLink_in_browser}
@@ -177,7 +177,7 @@ export default class App extends Component {
               <Text style={{ color: '#FFF', fontSize: 14 }}>Open Link in default Browser</Text>
             </TouchableOpacity> : null
           }
- 
+
           <TouchableOpacity
             onPress={this.open_QR_Code_Scanner}
             style={styles.button}>
@@ -193,13 +193,13 @@ export default class App extends Component {
               เปิดการติดตาม
             </Text>
           </TouchableOpacity>
- 
+
         </View>
       );
     }
     return (
       <View style={{ flex: 1 }}>
-        <Text style={{fontWeight: 'bold', textAlign: 'center'}}>เปิดเว็บไซต์ trackmycars.net/bike ทำการเข้าสู่ระบบ และสแกนคิวอาร์โค้ดของรถจักรยานยนต์ที่ท่านต้องการใช้งาน</Text>
+        <Text style={{ fontWeight: 'bold', textAlign: 'center' }}>เปิดเว็บไซต์ trackmycars.net/bike ทำการเข้าสู่ระบบ และสแกนคิวอาร์โค้ดของรถจักรยานยนต์ที่ท่านต้องการใช้งาน</Text>
 
         <CameraKitCameraScreen
           showFrame={true}
@@ -212,14 +212,14 @@ export default class App extends Component {
           actions={{ leftButtonText: 'ยกเลิก' }}
           onBottomButtonPressed={(event) => this.onBottomButtonPressed(event)}
         />
- 
+
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
- 
+
   MainContainer: {
     flex: 1,
     paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
